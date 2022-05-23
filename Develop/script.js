2056e610e32047847a545a31be99e03d227f3036
Leoman <leo.pinzon102@gmail.com>
2022-05-23 17:13:53 -0400

    all enhancements completed

--- Develop/script.js
+++ Develop/script.js
@@ -13,13 +13,22 @@ var generatePassword = function(){
       window.alert("Please, choose a length of at least 8 characters and no more than 128 characters")
       generatePassword();
     }
-
+    //call prompts to identify the type of characters required
     consolidatePassword();
+
+    // console.log(characterTypes.split(""));
+    // console.log(characterTypes);
     
+    //select random number from character types using as a limit passwordLength
     for (var i = 0; i < passwordLength; i++){
-      result += characterTypes[Math.floor(Math.random() * characterTypes.length)] 
+
+      result += characterTypes[Math.floor(Math.random() * characterTypes.split("").length)] 
     }
+
+    
+    //send "result" as the password for the user
     return result;
+   
   }
   
 }
@@ -29,66 +38,51 @@ var result = "";
 
 var consolidatePassword = function (){
   var lowerCasePromp = confirm("Do you want to include lowercase?");
+  // if yess add to characterTypes
   if(lowerCasePromp){
     characterTypes += "abcdefghijklmnopqrstuvwxyz";
   }
   var uperCasePromp = confirm("Do you want to include UPERCASE?");
+  // if yess add to characterTypes
   if(uperCasePromp){
     characterTypes += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   }
   var numericPromp = confirm("Do you want to include Numbers?");
+  // if yess add to characterTypes
   if(numericPromp){
     characterTypes += "0123456789";
   }
   var specialCharPromp = confirm("Do you want to include Special !#$%&'()*+,-./:;<=>?@[\]^_`{|}~?");
+  // if yess add to characterTypes
   if(specialCharPromp){
     characterTypes += "^$*.[]{}()?-\"!@#%&/\\,><':;|_~`";
   }
   
+  //if no type has been selected send an alert
   if(!lowerCasePromp && !uperCasePromp && !numericPromp && !specialCharPromp){
     alert("Must include at least one character type!");
     consolidatePassword();
   }
 }
 
-
-
-// var charTypes = "";
-// var numberOfTypes = 0;
-
-// var lowerCase = {
-//   options: "abcdefghijklmnopqrstuvwxyz",
-//   typePromp: confirm("Do you want to include lowercase?"),
-//   itemOptions: function(){
-//     if(this.typePromp){
-//       charTypes += "abcdefghijklmnopqrstuvwxyz";
-//       numberOfTypes +=1
-//     }
-//   } 
-// }
-
-// console.log(charTypes);
-
-
-// var consolidatePassword = function (){
-
-//   return charTypes;
-// };
-
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-
-
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  
+  if(!password){
+    generatePassword();
+  }else {
+  alert("Your password has been created successfully! Thanks for  using Password-Generator 🔐")
+  }
+  
+  
 }
 
 // Add event listener to generate button
